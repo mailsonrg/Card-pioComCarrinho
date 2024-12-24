38a571144971dca8985bca97bedc8ebc413818de
Mailson <51337178+mailsonrg@users.noreply.github.com>
2024-12-24 11:33:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 22;
+    return hora >= 07 && hora < 17;
     //true = restaurante está aberto
 }
 
